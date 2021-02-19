4b99bbe398d86310a05e09c612580ccf34fbece9
animsh <animsh.more@gmail.com>
2021-02-19 21:08:34 +0530

    changelog: designed recipe container placeholder layout

--- app/src/main/java/com/animsh/appita/RecipesFragment.kt
+++ app/src/main/java/com/animsh/appita/RecipesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class RecipesFragment : Fragment() {
 
@@ -13,6 +14,9 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        view.recipeRecyclerview.addItemDecoration(GridSpacingItemDecoration(2, 10, true))
+        view.recipeRecyclerview.showShimmer()
+        return view
     }
 }
