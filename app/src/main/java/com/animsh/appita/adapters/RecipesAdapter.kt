b986a29f49caa74d57fa0ec7922094672ad23d22
animsh <animsh.more@gmail.com>
2021-03-01 19:27:19 +0530

    changelog: added recipe adapter

---
+++ app/src/main/java/com/animsh/appita/adapters/RecipesAdapter.kt
@@ -0,0 +1,54 @@
+package com.animsh.appita.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.animsh.appita.databinding.LayoutRecipeContainerBinding
+import com.animsh.appita.models.FoodRecipe
+import com.animsh.appita.models.Result
+
+/**
+ * Created by animsh on 2/27/2021.
+ */
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.RecipeViewHolder>() {
+
+    private var recipes = emptyList<Result>()
+
+    class RecipeViewHolder(private val binding: LayoutRecipeContainerBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(result: Result) {
+            binding.result = result
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): RecipeViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = LayoutRecipeContainerBinding.inflate(layoutInflater, parent, false)
+                return RecipeViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecipesAdapter.RecipeViewHolder {
+        return RecipeViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: RecipesAdapter.RecipeViewHolder, position: Int) {
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
+    }
+
+    override fun getItemCount(): Int {
+        return recipes.size
+    }
+
+    fun setData(newData: FoodRecipe) {
+        recipes = newData.results
+        notifyDataSetChanged()
+    }
+
+}
