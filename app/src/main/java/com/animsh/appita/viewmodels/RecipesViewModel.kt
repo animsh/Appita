2e98f04b7fd30c95d0d75bc1d095aa8618b29305
animsh <animsh.more@gmail.com>
2021-03-15 21:16:45 +0530

    changelog: injected RecipeViewModel

--- app/src/main/java/com/animsh/appita/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/animsh/appita/viewmodels/RecipesViewModel.kt
@@ -1,7 +1,10 @@
 package com.animsh.appita.viewmodels
 
 import android.app.Application
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.animsh.appita.data.DataStoreRepository
 import com.animsh.appita.util.Constants.Companion.API_KEY
 import com.animsh.appita.util.Constants.Companion.DEFAULT_DIET_TYPE
 import com.animsh.appita.util.Constants.Companion.DEFAULT_MEAL_TYPE
@@ -15,16 +18,39 @@ import com.animsh.appita.util.Constants.Companion.QUERY_NUMBER
 import com.animsh.appita.util.Constants.Companion.QUERY_SORT
 import com.animsh.appita.util.Constants.Companion.QUERY_SORT_DIRECTION
 import com.animsh.appita.util.Constants.Companion.QUERY_TYPE
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class RecipesViewModel(application: Application) : AndroidViewModel(application) {
+class RecipesViewModel @ViewModelInject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+) : AndroidViewModel(application) {
+
+    var mealType = DEFAULT_MEAL_TYPE
+    var dietType = DEFAULT_DIET_TYPE
+
+    private val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) {
+        viewModelScope.launch {
+            dataStoreRepository.saveMealAndDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
+    }
 
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
+        viewModelScope.launch {
+            readMealAndDietType.collect {
+                mealType = it.selectedMealType
+                dietType = it.selectedDietType
+            }
+        }
+
         queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
-        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] = dietType
         queries[QUERY_INSTRUCTION_REQUIRED] = "true"
         queries[QUERY_SORT] = "popularity"
         queries[QUERY_SORT_DIRECTION] = "asc"
